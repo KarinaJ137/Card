d5316bd98e7adef5d2476a90675a70d49071495c
KarinaJ137 <k6513291@gmail.com>
2025-06-06 16:53:56 +0500

    Card

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export function Header() {
+    return(
+        <div className="header">
+            <h1 className="title">Флеш-карточки</h1>
+        </div>
+    );
+}
---
+++ src/components/Set.js
@@ -0,0 +1,10 @@
+import {Card} from "./Card";
+export function Set() {
+    return (
+        <div>
+            <h2 className="subTitle">Название набора</h2>
+            <Card />
+            <div>Элементы управления</div>
+        </div>
+    );
+}
